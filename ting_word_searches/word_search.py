7a5aaa7b2f1a76c999ee898b6b24dd1132fd25d2
Arthur Pelegrino <arthurpelegrino97@gmail.com>
2023-06-19 18:36:27 -0300

    Requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
 def exists_word(word, instance):
-    """Let's go!"""
+    new_dicionary = []
+    for file in instance.queue:
+        file_name = file["nome_do_arquivo"]
+        lines = file["linhas_do_arquivo"]
+        occurrences = [
+            {"linha": index + 1}
+            for index, line in enumerate(lines)
+            if word.lower() in line.lower()
+        ]
+
+        if occurrences:
+            new_dicionary.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_name,
+                    "ocorrencias": occurrences,
+                }
+            )
+
+    return new_dicionary
 
 
 def search_by_word(word, instance):
